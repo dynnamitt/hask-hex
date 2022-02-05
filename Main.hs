0ee4278049eb515e481269b2b0ef2e116e884783
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-02-05 11:20:16 +0100

    it runs magically, lazy infinite haskell style

--- Main.hs
+++ Main.hs
@@ -1,4 +1,16 @@
 module Main where
 
+import System.Random
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  gen' <- getStdGen
+  let widths = randInts gen' $ 5 - 1
+  putStrLn $ "w :" ++ show widths
+
+-- lazy , recursive
+randInts :: StdGen -> Int -> [Int]
+randInts g maxInt = 
+  let (n,g') = randomR (0,maxInt) g
+  in n : randInts g' maxInt
