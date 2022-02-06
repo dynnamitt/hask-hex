6a91d7b95a1a5e822377192fb02ae1a8bc0c6149
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-02-06 17:17:00 +0100

    interleave cycle zip >>= yay!

--- Main.hs
+++ Main.hs
@@ -2,16 +2,24 @@ module Main where
 
 import System.Random
 import Data.Char
+import Data.List
 
 seed = 2022
 
+screenLen :: Int
+screenLen = 80
 
 
 main :: IO ()
 main = do
+  let strLineLen = screenLen `div` 2
 
-  let cs = randChars $ mkStdGen seed
-  mapM_ putStrLn $ collectAsString 40 cs
+  let chars = randChars $ mkStdGen seed
+  let pureLines = chunkIntoString strLineLen chars
+  let empties = take strLineLen $ repeat ' '
+  let biState = cycle [True,False]
+  let hexLines = map (\(xs,skew) -> interleaveSkewed xs empties skew) $ zip pureLines biState
+  mapM_ putStrLn $ hexLines
 
 -- recursive generator
 randChars :: StdGen -> [Char]
@@ -19,8 +27,15 @@ randChars g =
   let (n,g') = randomR ('a','Æ') g
   in n : randChars g'
 
-collectAsString :: Int -> [Char] -> [String]
-collectAsString lineW cs =
+chunkIntoString :: Int -> [Char] -> [String]
+chunkIntoString lineW cs =
   let line = [take lineW cs_]
-  in line ++ collectAsString lineW (drop lineW cs)
+  in line ++ chunkIntoString lineW (drop lineW cs_)
   where cs_ = filter isPrint cs
+
+interleaveSkewed :: [a] -> [a] -> Bool -> [a]
+interleaveSkewed x y True = interleaveLists [x, y]
+interleaveSkewed x y False = interleaveLists [y, x]
+
+interleaveLists :: [[a]] -> [a]
+interleaveLists = concat . transpose
