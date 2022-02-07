7a5d5fee2b1f8311bac1587ca3271a34bef2f259
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-02-07 17:16:22 +0100

    zero.1 progress

--- Main.hs
+++ Main.hs
@@ -1,26 +1,48 @@
 module Main where
 
-import System.Random
+import System.Random (mkStdGen, StdGen, randomR)
 import Data.Char
-import Data.List
+import Data.List (zip, transpose)
+import Data.Sequence (mapWithIndex)
+import Data.Maybe
 
 seed = 2022
 screenLen = 80
 
-main :: IO ()
-main = do
-  let maxCols = screenLen `div` 2
-  let charStream = randChars $ mkStdGen seed
-  let rowStream = chunkIntoString maxCols charStream
-  mapM_ putStrLn $ hexedRows maxCols rowStream
+type Hexcode = Char
+
+{-
+    A B
+   S * ?
+    ? ?
+-}
+
+data HexCell = HexCell {
+  branchedFrom::Influencer, -- what is the cost?
+  branchedTimes::Int,
+  xPos::Int,
+  hexcode::Hexcode
+} deriving (Show)
+
+data Influencer = A HexCell
+                | B HexCell
+                | Side HexCell
+                | NoInflux
+                deriving (Show)
+
+-- hexedCellRow Nothing currRow =
+--   currRow':hexedCellRow
+--   where
+--     currRow' = mapWithIndex (\pos c -> HexCell NoInflux 0 pos c) currRow
+--     fn' itm acc = acc ++ [itm] -- place back into same order
 
 -- recursive generator
-randChars :: StdGen -> [Char]
-randChars g =
-  let (c, g') = randomR ('A','L') g
-  in c : randChars g'
+randHexcodes :: StdGen -> [Hexcode]
+randHexcodes g =
+  let (c, g') = randomR ('A','E') g
+  in c : randHexcodes g'
 
-chunkIntoString :: Int -> [Char] -> [String]
+chunkIntoString :: Int -> [Hexcode] -> [String]
 chunkIntoString lineW cs =
   let line = [take lineW cs_]
   in line ++ chunkIntoString lineW (drop lineW cs_)
@@ -29,7 +51,7 @@ chunkIntoString lineW cs =
 -- shew with "holes"
 hexedRows :: Int -> [String] -> [String]
 hexedRows maxCols rows =
-  let holes = take maxCols $ repeat ' '
+  let holes = replicate maxCols ' '
       bipolarRows = zip rows $ cycle [True,False]
   in map (\(row,skew) -> interleaveSkewed row holes skew) bipolarRows
   where
@@ -38,3 +60,11 @@ hexedRows maxCols rows =
 
 interleaveLists :: [[a]] -> [a]
 interleaveLists = concat . transpose
+
+
+main :: IO ()
+main = do
+  let maxCols = screenLen `div` 2
+  let hexcodes = randHexcodes $ mkStdGen seed
+  let rowStream = chunkIntoString maxCols hexcodes
+  mapM_ putStrLn $ hexedRows maxCols rowStream
