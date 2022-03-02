c3df8ab37f0978a8264601f9fadd30e88844b439
kjetil midtlie <kjetil.midtlie@gmail.com>
2022-03-02 18:53:03 +0100

    just removing some ranges in term-inf

--- TermInf.hs
+++ TermInf.hs
@@ -6,26 +6,34 @@ import Colors
 import Text.Printf
 import Data.Maybe
 import Data.Char
+import Data.List
 import qualified System.Console.Terminal.Size as TS
+
+hi = 3000
+spaceOccupied = 17
 
 main :: IO ()
 main = do
   win <- TS.size
   let (TS.Window _ w)  = fromJust win :: TS.Window Int
-  let cols = div w 16
+  let cols = div w spaceOccupied
   mapM_ putStrLn $ toRows cols charCodeTbl
 
 toRows :: Int -> [String] -> [String]
 toRows cols tbl
-  | length tbl > cols  = row' cols <> (toRows cols $ drop cols tbl)
-  | otherwise           = row' $ length tbl
+  | length tbl > cols  = row' cols : (toRows cols $ drop cols tbl)
+  | otherwise           = [row' $ length tbl]
   where
-    row' x = map (<>"\t") $ take x tbl
-
+    row' x = intercalate "\t" $ take x tbl
 
 charCodeTbl :: [String]
 charCodeTbl =
-  [ showN i <> " " <> bg16 False 1 <> "   " <> showC i  <> toNorm | i <- [1..1300]]
+  [ showN i <> " " <> bg16 False 1 <> "   " <> showChar i  <> toNorm
+          | i <- [1..hi], valid i]
+  where
+    showChar = fst . showC
+    valid i = not $ generalCategory (chr i) `elem` noUse
+    noUse = [NonSpacingMark,SpacingCombiningMark,Control,NotAssigned]
 
 showN :: Int -> String
 showN x =
@@ -33,9 +41,9 @@ showN x =
   where
     x' = show x
 
-showC :: Int -> String
+showC :: Int -> (String,GeneralCategory)
 showC x
-  | isPrint x' = x':[]
-  | otherwise = showLitChar x' ""
+  | isPrint x' = (x':"  " ,generalCategory x')
+  | otherwise  = (showLitChar x' "" , generalCategory x')
   where
     x' = chr x
